abd2b0cd6ac526007dd3edf081651d2bb6f60fc9
mdmunnaahmed <msdmunna77@gmail.com>
2022-12-10 15:14:55 +0600

    reward running

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,13 +37,6 @@
 				},
 			},
 			{
-				breakpoint: 767,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1,
-				},
-			},
-			{
 				breakpoint: 575,
 				settings: {
 					slidesToShow: 1,
